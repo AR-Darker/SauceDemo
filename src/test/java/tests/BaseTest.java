6099fc958af3619ceff5e98b72f11b06c0e5f1a8
AR_Darker <ishkredov2@gmail.com>
2023-07-14 21:11:53 +0300

    ParralelTests + ScreenShots + Listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.CartPage;
 import pages.Inventorypage;
 import pages.LoginPage;
@@ -20,14 +24,26 @@ public class BaseTest {
     LoginPage loginPage;
     Inventorypage inventoryPage;
     CartPage cartPage;
+    @Parameters({"browser"})
 
     @BeforeMethod
 
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public void setUp(@Optional("chrome") String browser, ITestContext testContext) {
+        if(browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            testContext.setAttribute("driver",driver);
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        }else{
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+            testContext.setAttribute("driver",driver);
+
+        }
+
 //не забудь иницализировать страницы
         loginPage = new LoginPage(driver);
         inventoryPage = new Inventorypage(driver);
---
+++ src/test/java/tests/TestListener.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    public void onTestStart(ITestResult result){
+        System.out.printf("Test started: %s \n ",result.getName());
+    }
+    public void onTestSuccess(ITestResult result) {
+        System.out.printf("Test success: %s \n ",result.getName());
+    }
+    public void onTestFailure(ITestResult result) {
+        WebDriver driver = (WebDriver)result.getTestContext().getAttribute("driver");
+        ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        System.out.printf("Test failure: %s \n ",result.getName());
+    }
+    //todo make screenshoot
+}
